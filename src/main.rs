276c5bacfbea55a43312af3b8834f4369c97322c
zzedddd <melegend.forever@protonmail.com>
2023-11-16 02:39:10 -0530

    Refactor main.rs: Update routes and init tracing

--- src/main.rs
+++ src/main.rs
@@ -3,25 +3,39 @@ use axum::{
     Router,
 };
 
-use sqlx::postgres::{PgPool, PgPoolOptions};
+use sqlx::postgres::PgPoolOptions;
 use tokio::net::TcpListener;
+
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
+
+mod controllers;
+mod models;
 
 #[tokio::main]
 async fn main() {
+    let database_url = std::env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+
+    tracing_subscriber::registry()
+        .with(tracing_subscriber::EnvFilter::new(
+            std::env::var("RUST_LOG").unwrap_or_else(|_| "blogrs=debug".into()),
+        ))
+        .with(tracing_subscriber::fmt::layer())
+        .init();
+
     let pool = PgPoolOptions::new()
         .max_connections(5)
-        .connect("postgres://postgres:postgres@localhost:5432")
+        .connect(&database_url)
         .await
         .expect("can't connect to database");
 
     let app = Router::new()
-        .route("/", get(|| async { "Hello, World!" }))
-        .route("/users", post(|| async { "Create user" }))
-        .route("/users/:id", patch(|| async { "Update user" }))
-        .route("/users/:id", delete(|| async { "Delete user" }))
+        .route("/", get(|| async { "Welcome to blogrs API!" }))
+        .route("/post/create", post(|| async { "Create Post" }))
+        .route("/post/edit/:id", patch(|| async { "Edit Post" }))
+        .route("/post/delete/:id", delete(|| async { "Delete Post" }))
         .with_state(pool);
 
     let listener = TcpListener::bind("127.0.0.1:3000").await.unwrap();
-    println!("Listening on {}", listener.local_addr().unwrap());
+    tracing::debug!("Listening on {}", listener.local_addr().unwrap());
     axum::serve(listener, app).await.unwrap();
 }
