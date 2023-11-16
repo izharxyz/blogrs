e267b816e2be1e5704b9fe16aafded9769d70426
zzedddd <melegend.forever@protonmail.com>
2023-11-16 02:49:10 -0530

    Add dotenv and CORS support

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,11 @@ use axum::{
     Router,
 };
 
+use dotenv::dotenv;
 use sqlx::postgres::PgPoolOptions;
 use tokio::net::TcpListener;
 
+use tower_http::cors::{Any, CorsLayer};
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
 mod controllers;
@@ -13,6 +15,8 @@ mod models;
 
 #[tokio::main]
 async fn main() {
+    dotenv().ok();
+
     let database_url = std::env::var("DATABASE_URL").expect("DATABASE_URL must be set");
 
     tracing_subscriber::registry()
@@ -28,12 +32,18 @@ async fn main() {
         .await
         .expect("can't connect to database");
 
+    let cors = CorsLayer::new()
+        // .allow_methods(vec!["GET", "POST", "PATCH", "DELETE"]) TODO: Fix this
+        .allow_origin(Any)
+        .allow_credentials(false);
+
     let app = Router::new()
         .route("/", get(|| async { "Welcome to blogrs API!" }))
         .route("/post/create", post(|| async { "Create Post" }))
         .route("/post/edit/:id", patch(|| async { "Edit Post" }))
         .route("/post/delete/:id", delete(|| async { "Delete Post" }))
-        .with_state(pool);
+        .with_state(pool)
+        .layer(cors);
 
     let listener = TcpListener::bind("127.0.0.1:3000").await.unwrap();
     tracing::debug!("Listening on {}", listener.local_addr().unwrap());
