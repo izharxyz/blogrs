0617d47b6fa4ecb7f01eb89ce87ffa243ed31d74
zzedddd <melegend.forever@protonmail.com>
2023-11-21 11:29:40 -0530

    Add modules and fix database connection error

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,12 @@
+mod controllers;
+mod model;
+mod schema;
+
 use axum::{
+    http::{
+        header::{ACCEPT, AUTHORIZATION, CONTENT_TYPE},
+        Method,
+    },
     routing::{delete, get, patch, post},
     Router,
 };
@@ -9,9 +17,6 @@ use tokio::net::TcpListener;
 
 use tower_http::cors::{Any, CorsLayer};
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
-
-mod controllers;
-mod models;
 
 #[tokio::main]
 async fn main() {
@@ -26,16 +31,26 @@ async fn main() {
         .with(tracing_subscriber::fmt::layer())
         .init();
 
-    let pool = PgPoolOptions::new()
+    let pool = match PgPoolOptions::new()
         .max_connections(5)
         .connect(&database_url)
         .await
-        .expect("can't connect to database");
+    {
+        Ok(pool) => {
+            tracing::info!("Connection to the database is successful!");
+            pool
+        }
+        Err(err) => {
+            tracing::error!("Failed to connect to the database: {:?}", err);
+            std::process::exit(1);
+        }
+    };
 
     let cors = CorsLayer::new()
-        // .allow_methods(vec!["GET", "POST", "PATCH", "DELETE"]) TODO: Fix this
+        .allow_methods([Method::GET, Method::POST, Method::PATCH, Method::DELETE])
         .allow_origin(Any)
-        .allow_credentials(false);
+        .allow_credentials(false)
+        .allow_headers([AUTHORIZATION, ACCEPT, CONTENT_TYPE]);
 
     let app = Router::new()
         .route("/", get(|| async { "Welcome to blogrs API!" }))
