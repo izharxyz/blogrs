01ee9deeec63cd9222904e88566dc5e050d9a26d
zzedddd <melegend.forever@protonmail.com>
2023-12-01 03:33:40 -0530

    Update PostModel and CategoryModel in model.rs

--- src/model.rs
+++ src/model.rs
@@ -1,21 +1,23 @@
 use serde::{Deserialize, Serialize};
+use sqlx::FromRow;
 
-#[derive(sqlx::FromRow, Serialize, Deserialize)]
+#[derive(Debug, FromRow, Deserialize, Serialize)]
+#[allow(non_snake_case)]
 pub struct PostModel {
     pub id: i32,
     pub title: String,
     pub slug: String,
-    pub author: Option<String>,
+    pub author_id: Option<i32>,
     pub excerpt: String,
     pub content: String,
-    pub category: Option<String>,
+    pub category_id: Option<i32>,
     #[serde(rename = "createdAt")]
     pub created_at: Option<chrono::DateTime<chrono::Utc>>,
     #[serde(rename = "updatedAt")]
     pub updated_at: Option<chrono::DateTime<chrono::Utc>>,
 }
 
-#[derive(sqlx::FromRow, Serialize, Deserialize)]
+#[derive(FromRow, Serialize, Deserialize)]
 pub struct CategoryModel {
     pub id: i32,
     pub name: String,
