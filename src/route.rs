f180ea76c0027d83f166efccc10a27bf3982113e
zzedddd <melegend.forever@protonmail.com>
2023-12-06 03:59:40 -0530

    Update route.rs to include new API endpoints

--- src/route.rs
+++ src/route.rs
@@ -1,11 +1,19 @@
 use std::sync::Arc;
 
-use axum::{routing::get, Router};
+use axum::{
+    routing::{get, patch, post},
+    Router,
+};
 
-use crate::{controller::fetch_post_controller, AppState};
+use crate::{
+    controller::{create_post_controller, fetch_post_controller, update_post_controller},
+    AppState,
+};
 
 pub fn api_routes(app_state: Arc<AppState>) -> Router {
     Router::new()
-        .route("/posts", get(fetch_post_controller))
+        .route("/post", get(fetch_post_controller))
+        .route("/post/create", post(create_post_controller))
+        .route("/post/update/:id", patch(update_post_controller))
         .with_state(app_state)
 }
