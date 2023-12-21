b650746ff05028edc1a2a0ea9e355fce51eddecc
zzedddd <melegend.forever@protonmail.com>
2023-12-21 09:05:40 -0530

    Add fetch_post_detail_handler to API routes

--- src/route.rs
+++ src/route.rs
@@ -6,13 +6,17 @@ use axum::{
 };
 
 use crate::{
-    handler::{create_post_handler, delete_post_handler, fetch_post_handler, update_post_handler},
+    handler::{
+        create_post_handler, delete_post_handler, fetch_post_detail_handler, fetch_post_handler,
+        update_post_handler,
+    },
     AppState,
 };
 
 pub fn api_routes(app_state: Arc<AppState>) -> Router {
     Router::new()
         .route("/post", get(fetch_post_handler))
+        .route("/post/:slug", get(fetch_post_detail_handler))
         .route("/post/create", post(create_post_handler))
         .route("/post/update/:id", patch(update_post_handler))
         .route("/post/delete/:id", delete(delete_post_handler))
